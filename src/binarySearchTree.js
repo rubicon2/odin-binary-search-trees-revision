ca2d05e06bf5f14a4dc342d8e8b6859bba40aaef
rubicon2 <liargent@hotmail.com>
2024-02-12 16:05:55 +0000

    Implement binary tree node

---
+++ src/binarySearchTree.js
@@ -0,0 +1,10 @@
+/* eslint-disable max-classes-per-file */
+class BinaryTreeNode {
+  constructor(data, leftNode, rightNode) {
+    this.data = data;
+    this.left = leftNode;
+    this.right = rightNode;
+  }
+}
+
+export default class BinarySearchTree {}
