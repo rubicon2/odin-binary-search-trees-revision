18d9f8576a9c8603ebdcfe5cb3e168db08bbffbb
rubicon2 <liargent@hotmail.com>
2024-02-14 16:18:18 +0000

    Add testing code
    As per "tie it all together" section on odin
    lesson for binary search trees.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import './style.css';
+
+import BinarySearchTree from './binarySearchTree';
+import { getRangedRandomInt, getRangedRandomIntArray } from './util';
+
+function printTreeStatus(tree) {
+  BinarySearchTree.prettyPrint(tree.root);
+  console.log('Is balanced: ', tree.isBalanced());
+
+  console.log('LEVEL ORDER:');
+  console.log(tree.levelOrder());
+
+  console.log('PRE ORDER:');
+  console.log(tree.preOrder());
+
+  console.log('POST ORDER:');
+  console.log(tree.postOrder());
+
+  console.log('IN ORDER:');
+  console.log(tree.inOrder());
+}
+
+const tree = new BinarySearchTree(getRangedRandomIntArray(0, 100, 50));
+printTreeStatus(tree);
+
+for (let i = 0; i < 50; i += 1) {
+  tree.insert(getRangedRandomInt(100, 1000));
+}
+
+printTreeStatus(tree);
+
+tree.rebalance();
+
+printTreeStatus(tree);
